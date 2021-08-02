859a355bacdd25ae4f571a6f8701a9a3b50c7259
Juansecu <juansecoder@hotmail.com>
2021-08-02 16:33:42 -0500

    Functionality for ChartService from ChartModule is done

--- src/app/chart/services/chart.service.ts
+++ src/app/chart/services/chart.service.ts
@@ -1,9 +1,15 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+
+import { Observable } from "rxjs";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class ChartService {
+  constructor(private readonly _http: HttpClient) {}
 
-  constructor() { }
+  getChartData(): Observable<object> {
+    return this._http.get("http://localhost:3000/chart");
+  }
 }
