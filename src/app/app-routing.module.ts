3f511c84c8d0c30ad1b80acf8027a68342353f12
Juansecu <juansecoder@hotmail.com>
2021-08-02 09:40:22 -0500

    Routes for ChartsModule were setted

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'charts',
+    loadChildren: () => import('./charts/charts.module').then(m => m.ChartsModule)
+  },
+  {
+    path: '**',
+    pathMatch: 'full',
+    redirectTo: 'charts'
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/charts/charts-routing.module.ts
+++ src/app/charts/charts-routing.module.ts
@@ -1,7 +1,39 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { BarsComponent } from './pages/bars/bars.component';
+import { DoubleBarsComponent } from './pages/double-bars/double-bars.component';
+import { PieComponent } from './pages/pie/pie.component';
+import { PieHttpComponent } from './pages/pie-http/pie-http.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: 'bars',
+        component: BarsComponent
+      },
+      {
+        path: 'double-bars',
+        component: DoubleBarsComponent,
+      },
+      {
+        path: 'pie',
+        component: PieComponent,
+      },
+      {
+        path: 'pie-http',
+        component: PieHttpComponent,
+      },
+      {
+        path: '**',
+        pathMatch: 'full',
+        redirectTo: 'bars'
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
