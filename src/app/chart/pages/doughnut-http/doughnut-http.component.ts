29de2334e677e1b8b9406056aef4964e4a1ac72d
Juansecu <juansecoder@hotmail.com>
2021-08-02 18:59:04 -0500

    Template for DoughnutHttpComponent was setted

--- src/app/chart/pages/doughnut-http/doughnut-http.component.ts
+++ src/app/chart/pages/doughnut-http/doughnut-http.component.ts
@@ -1,12 +1,34 @@
 import { Component, OnInit } from "@angular/core";
 
+import { ChartType } from "chart.js";
+import { Color, Label, MultiDataSet } from "ng2-charts";
+
+import { ChartService } from "../../services/chart.service";
+
 @Component({
   selector: "app-doughnut-http",
   templateUrl: "./doughnut-http.component.html",
   styles: [],
 })
 export class DoughnutHttpComponent implements OnInit {
-  constructor() {}
+  doughnutChartType: ChartType = "doughnut";
+  colors: Color[] = [
+    {
+      backgroundColor: ["#F7464A", "#46BFBD", "#FDB45C", "#949FB1", "#4D5360"],
+    },
+  ];
+  doughnutChartData: MultiDataSet = [];
+  doughnutChartLabels: Label[] = [];
 
-  ngOnInit(): void {}
+  constructor(private chartService: ChartService) {}
+
+  ngOnInit(): void {
+    this.chartService.getChartData().subscribe((data) => {
+      const keys = Object.keys(data);
+      const values = Object.values(data);
+
+      this.doughnutChartLabels = keys;
+      this.doughnutChartData = values;
+    });
+  }
 }
