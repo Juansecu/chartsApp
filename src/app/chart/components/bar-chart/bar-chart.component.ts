411ebc843fa034be812c248e83eeeeb15632139e
Juansecu <juansecoder@hotmail.com>
2021-08-02 12:24:42 -0500

    Template for BarChartComponent from ChartModule was setted

--- src/app/chart/components/bar-chart/bar-chart.component.ts
+++ src/app/chart/components/bar-chart/bar-chart.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+
+import { ChartDataSets, ChartOptions, ChartType } from 'chart.js';
+import { Label } from 'ng2-charts';
 
 @Component({
   selector: 'app-bar-chart',
@@ -7,10 +10,19 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class BarChartComponent implements OnInit {
+  @Input() barChartLabels!: Label[];
+  
+  @Input() horizontal: boolean = false;
+  @Input() barChartData: ChartDataSets[] = [];
+  
+  barChartType!: ChartType;
 
-  constructor() { }
+  barChartOptions: ChartOptions = {
+    responsive: true
+  };
+  barChartLegend = true;
 
   ngOnInit(): void {
+    this.barChartType = this.horizontal ? 'horizontalBar' : 'bar';
   }
-
 }
